036f3b0cb0a7a6ba54d5e4c9df339925796b80db
pmpuryaev <puryaev.pavel@gmail.com>
2022-08-26 01:30:12 +0300

    change MEDIA

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_COMPANY_IMAGE_DIR = 'company_images'
 MEDIA_SPECIALITY_IMAGE_DIR = 'speciality_images'
