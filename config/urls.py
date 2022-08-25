3b5602ce86149f9174380fe9f868aff6906ae3cd
pmpuryaev <puryaev.pavel@gmail.com>
2022-08-26 01:28:07 +0300

    add domen for VPS

--- config/urls.py
+++ config/urls.py
@@ -54,7 +54,5 @@ urlpatterns = [
     path('search/', search, name='search_page'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
